4326da45b40029a2ef0c3d900d39738a455bfa97
Renato Janser <renato.janser@gmail.com>
2021-01-24 10:35:07 -0300

    Validação de formulario

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,8 +1,18 @@
 <h1>Cadastra novo post</h1>
+
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('posts.store') }}" method="POST">
     @csrf
-    <input type="text" name="title" id="title" placeholder="Título">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdp"></textarea>
-
+    <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdp">
+        {{ old('content') }}
+    </textarea>
     <button type="submit">Enviar</button>
 </form>
